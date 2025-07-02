c297e64c1a5cb29613a6407fe86c1c1f8098c4e3
vishalbatra89 <vishalbatra89@gmail.com>
2025-07-02 21:55:55 +0530

    first commit

---
+++ src/main/java/demo/pipelines/PipelinesApplication.java
@@ -0,0 +1,13 @@
+package demo.pipelines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/pipelines/PipelinesApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.pipelines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
